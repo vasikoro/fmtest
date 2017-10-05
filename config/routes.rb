c21ff58074e5aae45a6f09861586c30cf1e5b686
vasikoro <vasi_koro@hotmail.com>
2017-10-05 13:46:44 +0300

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root 'pages#home'
+
+  get "products" =>  "pages#products", as: :products
+
+  get "theScience" =>  "pages#theScience", as: :theScience
+
+  get "aboutus" =>  "pages#aboutus", as: :aboutus
+
+  get "projects" =>  "pages#projects", as: :projects
+
+  get "journals" =>  "pages#journals", as: :journals
+
+  get "home" =>  "pages#home", as: :home
+
+  get "FAQ" =>  "pages#faq", as: :faq
+end
